1f96142ed68b897e8b736ddde94d704cf53394fa
Joseph <josephjohndemarco@gmail.com>
2020-06-21 11:30:41 -0400

    End lesson 1.

--- app.js
+++ app.js
@@ -15,6 +15,13 @@ var UIController = (function() {
 // Global App Controller
 var controller = (function(budgetCtrl, UICtrl) {
 
+    var ctrlAddItem = function() {
+        //1. Get field input data.
+        //2. Add item to budget controller.
+        //3. Add item to UI.
+        //4. Calculate budget.
+        //5. Display budget in UI.
+    }
 
     document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
 
@@ -22,6 +29,7 @@ var controller = (function(budgetCtrl, UICtrl) {
     document.addEventListener('keypress', function(event) {
         if (event.keyCode === 13 || event.which === 13) {
             // ENTER key is pressed.
+            ctrlAddItem();
 
         }
     });
