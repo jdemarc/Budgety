8d4e0addf307f19d47eb645764575d75d57edc65
Joseph <josephjohndemarco@gmail.com>
2020-06-22 12:57:09 -0400

    Manipulating HTML and placeholders to add items to UI.

--- app.js
+++ app.js
@@ -50,10 +50,6 @@ var budgetController = (function() {
 
             // Return new element.
             return newItem;
-        },
-
-        testing: function() {
-            console.log(data);
         }
     };
 
@@ -66,7 +62,9 @@ var UIController = (function() {
         inputType: '.add__type',
         inputDescription: '.add__description',
         inputValue: '.add__value',
-        inputBtn: '.add__btn'
+        inputBtn: '.add__btn',
+        incomeContainer: '.income__list',
+        expensesContainer: '.expenses__list'
     };
 
 
@@ -78,6 +76,32 @@ var UIController = (function() {
                 description: document.querySelector(DOMstrings.inputDescription).value,
                 value: document.querySelector(DOMstrings.inputValue).value
             };
+        },
+
+        addListItem: function (obj, type) {
+            var html, newHtml, element;
+
+            //1. Create HTML string with placeholder text.
+
+            if (type === 'inc') {
+                element = DOMstrings.incomeContainer;
+
+                html = '<div class="item clearfix" id="income-%id%"> <div class="item__description">%description%</div> <div class="right clearfix"><div class="item__value">%value%</div> <div class="item__delete"> <button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button> </div> </div> </div>';
+            } else if (type === 'exp') {
+                element = DOMstrings.expensesContainer;
+
+                html = '<div class="item clearfix" id="expense-%id%"> <div class="item__description">%description%</div> <div class="right clearfix"> <div class="item__value">%value%</div> <div class="item__percentage">21%</div> <div class="item__delete"> <button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button> </div> </div> </div>';
+            }
+
+            //2. Replace placeholder with actual data.
+            newHtml = html.replace('%id%', obj.id);
+            newHtml = newHtml.replace('%description%', obj.description);
+            newHtml = newHtml.replace('%value%', obj.value);
+
+            //3. Insert HTML into DOM.
+            document.querySelector(element).insertAdjacentHTML('beforeend', newHtml);
+
+
         },
 
         getDOMstrings: function() {
@@ -117,6 +141,7 @@ var controller = (function(budgetCtrl, UICtrl) {
         newItem = budgetCtrl.addItem(input.type, input.description, input.value);
 
         //3. Add item to UI.
+        UICtrl.addListItem(newItem, input.type);
 
         //4. Calculate budget.
 
