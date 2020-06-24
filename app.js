a078b469a2d64610179dae5f5b4ff30e2993a9e8
Joseph <josephjohndemarco@gmail.com>
2020-06-24 11:01:42 -0400

    Input validation. Refactoring function that will calc/update budget

--- app.js
+++ app.js
@@ -74,7 +74,7 @@ var UIController = (function() {
             return {
                 type: document.querySelector(DOMstrings.inputType).value, // INC or EXP
                 description: document.querySelector(DOMstrings.inputDescription).value,
-                value: document.querySelector(DOMstrings.inputValue).value
+                value: parseFloat(document.querySelector(DOMstrings.inputValue).value)
             };
         },
 
@@ -149,24 +149,35 @@ var controller = (function(budgetCtrl, UICtrl) {
     };
 
 
+    var updateBudget = function() {
+        //1. Calculate budget.
+
+        //2. Return budget.
+
+        //3. Display budget in UI.
+
+    };
+
     var ctrlAddItem = function() {
         var input, newItem;
 
         //1. Get field input data.
         input = UICtrl.getInput();
 
-        //2. Add item to budget controller.
-        newItem = budgetCtrl.addItem(input.type, input.description, input.value);
+        if (input.description !== "" && !isNaN(input.value) && input.value > 0) {
+            //2. Add item to budget controller.
+            newItem = budgetCtrl.addItem(input.type, input.description, input.value);
 
-        //3. Add item to UI.
-        UICtrl.addListItem(newItem, input.type);
+            //3. Add item to UI.
+            UICtrl.addListItem(newItem, input.type);
 
-        //4. Clear input fields.
-        UICtrl.clearFields();
+            //4. Clear input fields.
+            UICtrl.clearFields();
 
-        //5. Calculate budget.
+            //5. Calculate and update budget.
+            updateBudget();
+        }
 
-        //6. Display budget in UI.
     };
 
     return {
